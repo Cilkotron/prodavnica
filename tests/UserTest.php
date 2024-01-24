1a5d703632eff0b45ec6d266406f35e9acfb8e0f
Sanja Budic <sanjabudic2907@gmail.com>
2024-01-24 23:01:22 +0100

    Adding UserTest class

---
+++ tests/UserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+require_once 'src/bootstrap.php';
+
+class UserTest extends TestCase {
+
+    public function testnameIsValid() {
+        
+
+    }
+}
